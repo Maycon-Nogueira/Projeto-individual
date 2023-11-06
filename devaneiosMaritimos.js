a5fad92151475f0fb60518d3a3ec5ff07cf0cd2f
Maycon-Nogueira <maycon.nogueira@sptech.school>
2023-11-06 10:24:36 -0300

    Seção entrar quase pronta

--- devaneiosMaritimos.js
+++ devaneiosMaritimos.js
@@ -36,31 +36,56 @@ function rodarTexto(){
 // Salmão
     if (div_preparoSalmao.style.display != "flex"){
 
-        div_ingredientesSalmao.style.display = "none"
-        div_preparoSalmao.style.display = "flex"
+        div_ingredientesSalmao.style.display = "none";
+        div_preparoSalmao.style.display = "flex";
+        id_botaoIngreSalmao.style.display = "flex";
+        seta_esquerdaSalmao.style.display = "flex";
+        id_botaoPrepSalmao.style.display = "none";
+        seta_direitaSalmao.style.display= "none";
+
     } else {
         div_ingredientesSalmao.style.display = "flex";
         div_preparoSalmao.style.display = "none";
+        id_botaoPrepSalmao.style.display = "flex";
+        seta_direitaSalmao.style.display= "flex";
+        id_botaoIngreSalmao.style.display = "none";
+        seta_esquerdaSalmao.style.display = "none";
     }
 
 // Camarão
     if (div_preparoCamarao.style.display != "flex"){
 
-        div_ingredientesCamarao.style.display = "none"
-        div_preparoCamarao.style.display = "flex"
+        div_ingredientesCamarao.style.display = "none";
+        div_preparoCamarao.style.display = "flex";
+        id_botaoIngreCamarao.style.display = "flex";
+        seta_esquerdaCamarao.style.display = "flex";
+        id_botaoPrepCamarao.style.display = "none";
+        seta_direitaCamarao.style.display= "none";
     } else {
         div_ingredientesCamarao.style.display = "flex";
         div_preparoCamarao.style.display = "none";
+        id_botaoPrepCamarao.style.display = "flex";
+        seta_direitaCamarao.style.display= "flex";
+        id_botaoIngreCamarao.style.display = "none";
+        seta_esquerdaCamarao.style.display = "none";
     }
 
 // Carangueijo
     if (div_preparoCarangueijo.style.display != "flex"){
 
-        div_ingredientesCarangueijo.style.display = "none"
-        div_preparoCarangueijo.style.display = "flex"
+        div_ingredientesCarangueijo.style.display = "none";
+        div_preparoCarangueijo.style.display = "flex";
+        id_botaoIngreCarangueijo.style.display = "flex";
+        seta_esquerdaCarangueijo.style.display = "flex";
+        id_botaoPrepCarangueijo.style.display = "none";
+        seta_direitaCarangueijo.style.display= "none";
     } else {
         div_ingredientesCarangueijo.style.display = "flex";
         div_preparoCarangueijo.style.display = "none";
+        id_botaoPrepCarangueijo.style.display = "flex";
+        seta_direitaCarangueijo.style.display= "flex";
+        id_botaoIngreCarangueijo.style.display = "none";
+        seta_esquerdaCarangueijo.style.display = "none";
     }
 
 };
@@ -68,3 +93,18 @@ function rodarTexto(){
 
 
 // Fim seção receitas
+
+// Começo seção Entrar
+function abrirFecharLogin(){
+    if (div_cadastro.style.display == "none"){
+        div_cadastro.style.display = "flex";
+        div_login.style.display = "none";
+    } else {
+        div_cadastro.style.display = "none";
+        div_login.style.display = "flex";
+    }
+
+
+
+}
+// Fim seção Entrar
